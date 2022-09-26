741d1c2b958dabe64943e09e54b5d01352de34dd
Chanu <nugin2010@gmail.com>
2022-09-26 20:27:00 +0700

    add api upload file

--- app.js
+++ app.js
@@ -65,3 +65,102 @@ app.get("/profiles/:id", (req, res) => {
     });
   else res.send({ code: 500, msg: "Please add id" });
 });
+
+// Upload File and mysq Line notify
+app.post("/insert", (req, res) => {
+  const newpath = __dirname + "/uploads/";
+  const file = req.files.upload;
+  const filename = file.name;
+
+  const urls = req.body.urls;
+  const userid = req.body.userid;
+  const name = req.body.name;
+  const email = req.body.email;
+  const tel = req.body.tel;
+  const policy = req.body.policy;
+  const question = req.body.question;
+  const others = JSON.parse(req.body.others);
+  const txtother = others.map((item) => item.value);
+  const urlProfile = urls + "/meso-questions/profiles/" + userid;
+  var imgsrc = newpath + filename;
+
+  let txtquestion = "";
+  const questionTitle = [
+    { id: 1, title: "1. คุณอยู่ในช่วงอายุเท่าไร?" },
+    { id: 2, title: "2. งบประมาณในการรักษาฝ้าให้หาย?" },
+    { id: 3, title: "3. ปัญหาผิวอื่นๆ ที่ท่านมี?" },
+    { id: 4, title: "4. สิ่งที่ท่านกังวลในการรักษาฝ้า " },
+    { id: 5, title: "5. เวลาที่สะดวกในการโทรติดต่อ" },
+  ];
+  const date = req.body.date;
+  if (txtother.length === 0) {
+    txtother.push(..."-");
+  }
+  const questionNew = JSON.parse(req.body.question);
+  for (let value1 of Object.values(questionTitle)) {
+    for (let value2 of Object.values(questionNew)) {
+      if (value1.id == value2.id) {
+        txtquestion =
+          txtquestion + ":" + value1.title + " : " + value2.answer + "\n";
+      }
+    }
+  }
+
+  console.log("======== newpath ======== ", newpath);
+  // console.log("======== filename ======== ", filename);
+  // console.log("======= url ======", urls);
+  // console.log("================= userid : ", userid);
+  // console.log("================= name : ", name);
+  // console.log("================= email : ", email);
+  // console.log("================= tel : ", tel);
+  // console.log("================= policy : ", policy);
+  // console.log("================= question : ", question);
+  // console.log("================= date : ", date);
+  // console.log("================= txtquestion", txtquestion);
+  // console.log("================= others", others);
+  // console.log("================= txtother", txtother);
+  // console.log("================= imgsrc", imgsrc);
+  file.mv(`${newpath}${filename}`, (err) => {
+    if (err) {
+      res.send({ code: 500, msg: "errrrrrr" });
+    }
+    //   // //================= Line notifile =================
+    const lineNotify = require("line-notify-nodejs")(
+      "VP66e2N9FxbS1g7cxJPXpcmbp52pipEAZGWCv87b4tf"
+    );
+    lineNotify
+      .notify({
+        message:
+          "\n===== แบบสอบถาม ===== \n" +
+          "ชื่อ-นามสกุล : " +
+          name +
+          "\n" +
+          "E-mail : " +
+          email +
+          "\n" +
+          "เบอร์โทรศัพท์ : " +
+          tel +
+          "\n" +
+          "แบบสอบถาม : " +
+          "\n" +
+          txtquestion +
+          "ข้อมูลเพิ่มเติม : " +
+          "\n" +
+          txtother +
+          "\n\n===============\n" +
+          "Link :" +
+          urlProfile,
+      })
+      .then(() => {
+        console.log("send completed!");
+      });
+    db.query(
+      "INSERT INTO question (name, email, tel, policy, question, upload,others,date) VALUES (?,?,?,?,?,?,?,?)",
+      [name, email, tel, policy, question, filename, txtother, date],
+      (err, result) => {
+        if (err) throw err;
+      }
+    );
+    res.send({ code: 200, msg: "Upload success" });
+  });
+});
